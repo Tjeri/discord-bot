3dd9c221cad42b2096d0a711af29bd1214e69eb1
Tjeri <tjeri@hadiko.de>
2021-09-02 02:50:01 +0200

    add missing 'args' option to constructor

--- commands/music/title.js
+++ commands/music/title.js
@@ -20,6 +20,14 @@ module.exports = class Play extends Command {
             group: 'music',
             description: 'changes the title of the current played title (and loop if it\'s the same)',
             guildOnly: true,
+            args: [
+                {
+                    key: 'title',
+                    prompt: 'What should the new title be?',
+                    type: 'string',
+                    validate: query => query.length > 0
+                }
+            ],
             throttling: {
                 usages: 1,
                 duration: 3
@@ -27,7 +35,7 @@ module.exports = class Play extends Command {
         });
     }
 
-    run = async (message, { query }) => {
+    run = async (message, { title }) => {
         try {
             if (!message.guild.music.nowPlaying) {
                 const embed = new MessageEmbed()
@@ -35,13 +43,13 @@ module.exports = class Play extends Command {
                     .setTitle(':musical_note: Nothing is playing right now');
                 return await message.say({ embed });
             }
-            message.guild.music.nowPlaying.title = query;
+            message.guild.music.nowPlaying.title = title;
             if (message.guild.music.loop === message.guild.music.nowPlaying) {
-                message.guild.music.loop.title = query;
+                message.guild.music.loop.title = title;
             }
             const embed = new MessageEmbed()
                 .setColor('#000099')
-                .setTitle(`:new: Title is now ${query}`);
+                .setTitle(`:new: Title is now ${title}`);
             return await message.say({ embed });
         } catch(err) {
             logger.log('error', err);
